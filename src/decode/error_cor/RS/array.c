6c11128c3ec3c473d73da0b7ec1e79e11a4dbe51
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2021-04-17 16:22:05 +0200

    Updated error correction for decoding.

--- src/decode/error_cor/RS/array.c
+++ src/decode/error_cor/RS/array.c
@@ -2,48 +2,48 @@
 
 void initArray(struct Array *a, size_t initialSize)
 {
-  a->array = malloc(sizeof(uint8_t) * initialSize);
-  if(a->array == NULL){
-    free(a);
-  }
-  a->used = 0;
-  a->size = initialSize;
+    a->array = malloc(sizeof(uint8_t) * initialSize);
+    if(a->array == NULL){
+        free(a);
+    }
+    a->used = 0;
+    a->size = initialSize;
 }
 
 void initZArray(struct Array *a, size_t initialSize)
 {
-  a->array = calloc(initialSize, sizeof(uint8_t));
-  if(a->array == NULL){
-    free(a);
-  }
-  a->used = 0;
-  a->size = initialSize;
+    a->array = calloc(initialSize, sizeof(uint8_t));
+    if(a->array == NULL){
+        free(a);
+    }
+    a->used = 0;
+    a->size = initialSize;
 }
 
 void insertArray(struct Array *a)
 {
-  if(a->used == a->size)
+    if(a->used == a->size)
     {
-      a->size *= 2;
-      void *p = realloc(a->array, a->size * sizeof(uint8_t));
-      if(p != NULL)
-        a->array = p;
-      else
-        exit(EXIT_FAILURE);
+        a->size *= 2;
+        void *p = realloc(a->array, a->size * sizeof(uint8_t));
+        if(p != NULL)
+            a->array = p;
+        else
+            exit(EXIT_FAILURE);
     }
-  a->used++;
+    a->used++;
 }
 
 void deleteArray(struct Array *a)
 {
-  a->used--;
+    a->used--;
 }
 
 
 void freeArray(struct Array *a)
 {
-  free(a->array);
-  a->array = NULL;
-  a->used = 0;
-  a->size = 0;
+    free(a->array);
+    a->array = NULL;
+    a->used = 0;
+    a->size = 0;
 }
